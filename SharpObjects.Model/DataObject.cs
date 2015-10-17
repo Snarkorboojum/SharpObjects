f0e6ea24aeb71b2903a7d7b9d7d99699e2f7c6dd
Razmik Seysyan <razmikdev@outlook.com>
2015-10-18 02:00:13 +0300

    Defines DataObjectValue with explicit layout

---
+++ SharpObjects.Model/DataObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SharpObjects.Model
+{
+	public class DataObject : Dictionary<String, Object>
+	{
+	}
+
+	[StructLayout(LayoutKind.Explicit, Pack = 1)]
+	public struct DataObjectValue
+	{
+		[FieldOffset(0)]
+		private DataObjectValueType _type;
+
+		[FieldOffset(sizeof(DataObjectValueType))]
+		private Boolean _booleanValue;
+
+		[FieldOffset(sizeof(DataObjectValueType))]
+		private Int32 _intValue;
+
+		[FieldOffset(sizeof(DataObjectValueType) + sizeof(Int32))]
+		private Object _referenceTypeValue;
+	}
+
+	internal enum DataObjectValueType : byte
+	{
+		Integer,
+		Float,
+		String,
+		Object
+	}
+}
