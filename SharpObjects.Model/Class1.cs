a74995d8ad1b9662699059956bfbb6c1251aefd0
Razmik Seysyan <razmikdev@outlook.com>
2015-10-18 01:37:10 +0300

    Adds solution with first project

---
+++ SharpObjects.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpObjects.Model
+{
+    public class DataOboject : Dictionary<String, Object>
+    {
+    }	
+}
