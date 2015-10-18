a304deb32d15886a3e5de34d215cdec7bf48cf21
Razmik Seysyan <razmikdev@outlook.com>
2015-10-18 16:53:25 +0300

    Implements base GetPropertyValue and SetPropertyValue methods for DataObject

--- SharpObjects.Model/DataObject.cs
+++ SharpObjects.Model/DataObject.cs
@@ -3,7 +3,25 @@ using System.Collections.Generic;
 
 namespace SharpObjects.Model
 {
-	public class DataObject : Dictionary<String, Object>
+	public class DataObject : Dictionary<String, DataObjectValue>
 	{
+		public DataObjectValue GetPropertyValue(String propertyName)
+		{
+			if (propertyName == null)
+				throw new ArgumentNullException(nameof(propertyName));
+
+			DataObjectValue result;
+			return TryGetValue(propertyName, out result)
+				? result
+				: default(DataObjectValue);
+		}
+
+		public void SetPropertyValue(String propertyName, DataObjectValue propertyValue)
+		{
+			if (propertyName == null)
+				throw new ArgumentNullException(nameof(propertyName));
+
+			this[propertyValue] = propertyValue;
+		}
 	}
 }
