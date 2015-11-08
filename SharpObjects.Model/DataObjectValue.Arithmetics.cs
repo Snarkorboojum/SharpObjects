8eb6f05723f88bd41039b4de16be406cea62bba4
Razmik Seysyan <razmikdev@outlook.com>
2015-11-08 19:47:32 +0300

    Adds arithmetic opetators like add and substract

---
+++ SharpObjects.Model/DataObjectValue.Arithmetics.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace SharpObjects.Model
+{
+	public partial struct DataObjectValue
+	{
+		[PublicAPI]
+		public DataObjectValue Negate()
+		{
+			switch (_type)
+			{
+				case DataObjectValueType.Boolean:
+				case DataObjectValueType.BooleanString:
+					return new DataObjectValue(!_booleanValue);
+
+				case DataObjectValueType.Integer:
+				case DataObjectValueType.IntegerString:
+					return new DataObjectValue(-_intValue);
+
+				case DataObjectValueType.Float:
+				case DataObjectValueType.FloatString:
+					return new DataObjectValue(-_singleValue);
+
+				default:
+					return this;
+			}
+		}
+
+		[PublicAPI]
+		public DataObjectValue Add(DataObjectValue other)
+		{
+			if (_type == DataObjectValueType.None)
+				return other;
+
+			if (other._type == DataObjectValueType.None)
+				return this;
+
+			if (_type.HasFlagFast(DataObjectValueType.Boolean))
+				return other._type.HasFlagFast(DataObjectValueType.Boolean)
+					? new DataObjectValue(_booleanValue | other._booleanValue)
+					: other;
+
+			if (other._type.HasFlagFast(DataObjectValueType.Boolean))
+				return this;
+
+			if (_type == DataObjectValueType.String)
+				return new DataObjectValue((String)_referenceTypeValue + (String)other);
+
+			if (other._type == DataObjectValueType.String)
+				return new DataObjectValue((String)this + (String)other._referenceTypeValue);
+
+			switch (_type)
+			{
+				case DataObjectValueType.Integer:
+				case DataObjectValueType.IntegerString:
+					{
+						if (other._type.HasFlagFast(DataObjectValueType.Integer))
+							return new DataObjectValue(_intValue + other._intValue);
+
+						if (other._type.HasFlagFast(DataObjectValueType.Float))
+							return new DataObjectValue(_intValue + other._singleValue);
+					}
+					break;
+
+				case DataObjectValueType.Float:
+				case DataObjectValueType.FloatString:
+					{
+						if (other._type.HasFlagFast(DataObjectValueType.Float))
+							return new DataObjectValue(_singleValue + other._singleValue);
+
+						if (other._type.HasFlagFast(DataObjectValueType.Integer))
+							return new DataObjectValue(_singleValue + other._intValue);
+					}
+					break;
+			}
+
+			return this;
+		}
+
+		[PublicAPI]
+		public DataObjectValue Substact(DataObjectValue other)
+		{
+			if (_type == DataObjectValueType.None)
+				return Nothing;
+
+			if (other._type == DataObjectValueType.None)
+				return this;
+
+			if (_type.HasFlagFast(DataObjectValueType.Boolean))
+			{                                                                               // false - false	=	false
+				return other._type.HasFlagFast(DataObjectValueType.Boolean)                 // false - true		=	false
+					? new DataObjectValue(_booleanValue && !other._booleanValue)            // true  - false	=	true
+					: other;                                                                // true  - true		=	false
+			}
+
+			if (other._type.HasFlagFast(DataObjectValueType.Boolean))
+				return this;
+
+			switch (_type)
+			{
+				case DataObjectValueType.Integer:
+				case DataObjectValueType.IntegerString:
+					{
+						if (other._type.HasFlagFast(DataObjectValueType.Integer))
+							return new DataObjectValue(_intValue - other._intValue);
+
+						if (other._type.HasFlagFast(DataObjectValueType.Float))
+							return new DataObjectValue(_intValue - other._singleValue);
+					}
+					break;
+
+				case DataObjectValueType.Float:
+				case DataObjectValueType.FloatString:
+					{
+						if (other._type.HasFlagFast(DataObjectValueType.Float))
+							return new DataObjectValue(_singleValue - other._singleValue);
+
+						if (other._type.HasFlagFast(DataObjectValueType.Integer))
+							return new DataObjectValue(_singleValue - other._intValue);
+					}
+					break;
+			}
+
+			return Equals(other)
+				? Zero
+				: this;
+		}
+
+		#region Unary operators
+
+		public static DataObjectValue operator -(DataObjectValue dataObjectValue)
+		{
+			return dataObjectValue.Negate();
+		}
+
+		#endregion
+
+		#region Binary opetators
+
+		public static DataObjectValue operator +(DataObjectValue summand1, DataObjectValue summand2)
+		{
+			return summand1.Add(summand2);
+		}
+
+		public static DataObjectValue operator -(DataObjectValue minuend, DataObjectValue subtrahend)
+		{
+			return minuend.Substact(subtrahend);
+		}
+
+		#endregion
+	}
+}
